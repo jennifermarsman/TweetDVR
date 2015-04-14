1957e914cf5c2646797ae4a6b5ccff84c9453001
matt winkler <mwinkle@microsoft.com>
2015-04-14 11:58:14 -0700

    additonal edits

--- archive_app/SocialArchiveApplication/Program.cs
+++ archive_app/SocialArchiveApplication/Program.cs
@@ -1,8 +1,20 @@
-﻿using System;
+﻿// inspired by Maxim's awesome sentiment sample
+// https://github.com/maxluk/tweet-sentiment/blob/master/tweet-sentiment/SimpleStreamingService/Program.cs 
+
+
+
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tweetinvi; 
+using Tweetinvi.Core.Enum; 
+using Tweetinvi.Core.Interfaces.Models; 
+using Tweetinvi.Core.Interfaces.Models.Parameters; 
+
 
 namespace Microsoft.Azure.HDInsight.Sample.SocialArchiveApplication
 {
@@ -10,6 +22,12 @@ namespace Microsoft.Azure.HDInsight.Sample.SocialArchiveApplication
     {
         static void Main(string[] args)
         {
+             TwitterCredentials.SetCredentials( 
+                 ConfigurationManager.AppSettings["token_AccessToken"], 
+                 ConfigurationManager.AppSettings["token_AccessTokenSecret"], 
+                 ConfigurationManager.AppSettings["token_ConsumerKey"], 
+                 ConfigurationManager.AppSettings["token_ConsumerSecret"]); 
+
         }
     }
 }
