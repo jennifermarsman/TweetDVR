c8f22e7b8f606d0ff9ad8e41e1eb95155b583036
Adam Comella <adcom@microsoft.com>
2015-04-14 17:46:05 -0700

    Integrate ToolBar into UI

--- TweetDVRWebAPI/TweetDVRWebAPI/Scripts/default.js
+++ TweetDVRWebAPI/TweetDVRWebAPI/Scripts/default.js
@@ -1,18 +1,26 @@
 ﻿(function () {
     // http://localhost:5848/api/tweetsapi?topic=GameOfThrones&time=2015-04-14&maxCount=100
-    window.doFetch = function () {
-        debugger;
-        WinJS.xhr({
-            url: "http://localhost:5848/api/tweetsapi?topic=GameOfThrones&time=2015-04-14",
-            responseType: "json"
-        }).then(function (arg) {
-            debugger;
-        }, function () {
-            debugger;
-        });
-    };
 
     WinJS.Namespace.define("App", {
+        // State
+        //
+        modes: {
+            play: {
+                icon: "play",
+                label: "Play",
+                tooltip: "Play the video"
+            },
+            pause: {
+                icon: "pause",
+                label: "Pause",
+                tooltip: "Pause the video"
+            }
+        },
+        hashtags: new WinJS.Binding.List([
+            { htName: '#GoT', isSelected: true },
+            { htName: '#DrWho', isSelected: false },
+            { htName: '#buildwindows', isSelected: false }
+        ]),
         currentTime: new Date(),
         list: new WinJS.Binding.List([{
             "IdStr": null,
@@ -38,16 +46,68 @@
             "ScreenName": "anyelwae"
         }]),
 
+        // Functions
+        //
         fetch: function (date) {
             var url = "http://localhost:5848/api/tweetsapi?topic=GameOfThrones&time=" + date.toISOString() + "&maxCount=100";
             return WinJS.xhr({
                 url: url,
                 responseType: "json"
             });
+        },
+        positiveSelected: WinJS.UI.eventHandler(function () {
+            context.model.isPositiveSelected = true;
+            context.model.isNegativeSelected = false;
+            context.model.isNeutralSelected = false;
+        }),
+        negativeSelected: WinJS.UI.eventHandler(function () {
+            context.model.isPositiveSelected = false;
+            context.model.isNegativeSelected = true;
+            context.model.isNeutralSelected = false;
+
+        }),
+        neutralSelected: WinJS.UI.eventHandler(function () {
+            context.model.isPositiveSelected = false;
+            context.model.isNegativeSelected = false;
+            context.model.isNeutralSelected = true;
+        }),
+        togglePlayPause: WinJS.UI.eventHandler(function (evt) {
+            context.model.currentMode = (context.model.currentMode.icon === App.modes.play.icon) ? App.modes.pause : App.modes.play;
+
+            if (contex.model.currentMode === App.modes.play) {
+                // Call the Web Service
+
+
+            };
+        }),
+        toggleHashtag: WinJS.UI.eventHandler(function (evt) {
+            var hashtags = App.hashtags;
+            var chk = evt.currentTarget;
+            var hashtag = chk.value;
+
+            for (var i = 0; i < hashtags.length; i++) {
+                if (hashtags.getAt(i).htName === hashtag) {
+                    hashtags.getAt(i).isSelected = chk.checked;
+                    hashtags.setAt(i, hashtags.getAt(i));
+                }
+            }
+
+        }),
+    });
+
+    var context = WinJS.Binding.as({
+        model: {
+            isPositiveSelected: false,
+            isNeutralSelected: true,
+            isNegativeSelected: false,
+            currentMode: App.modes.play,
+            tweetDate: 'April 13, 2015',
+            tweetTime: '9:00 PM',
         }
     });
 
     WinJS.UI.processAll().then(function () {
+        WinJS.Binding.processAll(document.body, context);
         App.fetch(new Date(2015, 3, 14)).then(function (arg) {
             arg.response.forEach(function (entry) {
                 App.list.unshift(entry);
