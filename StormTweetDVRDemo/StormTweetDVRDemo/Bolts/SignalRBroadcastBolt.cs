fec02d18dc65cd3757c5d83b54e2f8c2ee4a916f
Jennifer Marsman <jennmar@microsoft.com>
2015-04-15 02:23:14 -0400

    Initial checkin of Storm data flow from Twitter to SignalR.  No HBase integration yet.

---
+++ StormTweetDVRDemo/StormTweetDVRDemo/Bolts/SignalRBroadcastBolt.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.SCP;
+using Microsoft.AspNet.SignalR.Client;
+using System.Diagnostics;
+using System.Configuration;
+
+namespace StormTweetDVRDemo.Bolts
+{
+    public class SignalRBroadcastBolt : ISCPBolt
+    {
+        Context context;
+
+        //SingnalR Connection
+        HubConnection hubConnection;
+        IHubProxy twitterHubProxy;
+        Stopwatch timer = Stopwatch.StartNew();
+
+        //Constructor
+        public SignalRBroadcastBolt(Context context)
+        {
+            Context.Logger.Info("SignalRBroadcastBolt constructor called");
+            //Set context
+            this.context = context;
+
+            //Define the schema for the incoming tuples from spout
+            Dictionary<string, List<Type>> inputSchema = new Dictionary<string, List<Type>>();
+            //Input schema counter updates
+            //inputSchema.Add(Constants.DEFAULT_STREAM_ID, new List<Type>() { typeof(long), typeof(string) });
+            inputSchema.Add(Constants.DEFAULT_STREAM_ID, new List<Type>() { typeof(string), typeof(string), typeof(string), typeof(DateTime), typeof(bool),
+            typeof(string), typeof(bool), typeof(string), typeof(string), typeof(string), typeof(int), typeof(int), typeof(string), typeof(long), typeof(string)});
+
+
+            //Declare both incoming and outbound schemas
+            this.context.DeclareComponentSchema(new ComponentStreamSchema(inputSchema, null));
+
+            // Initialize SignalR connection
+            StartSignalRHubConnection();
+        }
+
+        //Get a new instance
+        public static SignalRBroadcastBolt Get(Context ctx, Dictionary<string, Object> parms)
+        {
+            return new SignalRBroadcastBolt(ctx);
+        }
+
+        //Process a tuple from the stream
+        public void Execute(SCPTuple tuple)
+        {
+            Context.Logger.Info("Execute enter");
+
+            var creatorName = tuple.GetValue(0) as string;
+            var creatorScreenName = tuple.GetValue(1) as string;
+            var createProfileImageUrl = tuple.GetValue(2) as string;
+            var createdAt = (DateTime) tuple.GetValue(3);
+            var isRetweet = (bool) tuple.GetValue(4);
+            var language = tuple.GetValue(5) as string;
+            var retweeted = (bool)tuple.GetValue(6);
+            var text = tuple.GetValue(7) as string;
+            var idStr = tuple.GetValue(8) as string;
+            var topic = tuple.GetValue(9) as string;
+            var retweetCount = (int) tuple.GetValue(10);
+            var favouriteCount = (int) tuple.GetValue(11);
+            var hashtags = tuple.GetValue(12) as string;
+            var orderIndex = (long)tuple.GetValue(13);
+            var sentiment = tuple.GetValue(14) as string;
+
+            try
+            {
+                //Only send updates every 500 milliseconds
+                //Ignore the messages in between so that you don't overload the SignalR website with updates at each tuple
+                //If you have only aggreagates to send that can be spaced, you don't need this timer
+                if (timer.ElapsedMilliseconds >= 100)
+                {
+                    SendSingnalRUpdate(orderIndex, "Jen! " + text);
+                    timer.Restart();
+                }
+            }
+            catch (Exception ex)
+            {
+                Context.Logger.Error("SignalRBroadcastBolt Exception: " + ex.Message + "\nStackTrace: \n" + ex.StackTrace);
+            }
+
+            Context.Logger.Info("Execute exit");
+        }
+
+        private void StartSignalRHubConnection()
+        {
+            //TODO: Specify your SignalR website settings in SCPHost.exe.config
+            this.hubConnection = new HubConnection(ConfigurationManager.AppSettings["SignalRWebsiteUrl"]);
+            this.twitterHubProxy = hubConnection.CreateHubProxy(ConfigurationManager.AppSettings["SignalRHub"]);
+            hubConnection.Start().Wait();
+        }
+
+        private void SendSingnalRUpdate(long rowCount, string tweet)
+        {
+            if (hubConnection.State != ConnectionState.Connected)
+            {
+                hubConnection.Stop();
+                StartSignalRHubConnection();
+            }
+            twitterHubProxy.Invoke("UpdateCounter", rowCount, tweet);       /// TODO Jen: change this
+        }
+    }
+}
---
+++ StormTweetDVRDemo/StormTweetDVRDemo/SerializableTweet.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Tweetinvi.Core.Interfaces;
+
+namespace StormTweetDVRDemo
+{
+    //Workaround to make the Tweet from TweetInvi Serializable
+    //Mark this class Serializable
+    [Serializable]
+    public class SerializableTweet
+    {
+
+        public string CreatorScreenName { get; set; }      // i.e. mwinkle
+        public string CreatorProfileImageUrl { get; set; }
+        public string CreatorName { get; set; }            // i.e. Matt Winkler
+
+        public DateTime CreatedAt { get; set; }
+        public bool IsRetweet { get; set; }
+        public string Language { get; set; }
+        public bool Retweeted { get; set; }
+        public string Text { get; set; }
+        public string IdStr { get; set; }
+        public string Topic { get; set; }
+        public int RetweetCount { get; set; }
+        public int FavouriteCount { get; set; }
+        public string Hashtags { get; set; }
+        public long OrderIndex { get; set; }
+        public string Sentiment { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tweet"></param>
+        /// <param name="topic"></param>
+        /// <param name="hashtags">Comma-separated list of hashtags</param>
+        public SerializableTweet(ITweet tweet, string topic, string hashtags)
+        {
+            this.CreatorName = tweet.Creator.Name;
+            this.CreatorScreenName = tweet.Creator.ScreenName;
+            this.CreatorProfileImageUrl = tweet.Creator.ProfileImageUrl;
+            this.CreatedAt = tweet.CreatedAt;
+            this.IsRetweet = tweet.IsRetweet;
+            this.Language = tweet.Language.ToString();
+            this.Retweeted = tweet.Retweeted;
+            this.Text = tweet.Text;
+            this.IdStr = tweet.IdStr;
+            this.Topic = topic;
+            this.RetweetCount = tweet.RetweetCount;
+            this.FavouriteCount = tweet.FavouriteCount;
+            this.Hashtags = hashtags;
+            this.OrderIndex = 0; // TODO: logic Jen come back and fix this
+            this.Sentiment = this.Sentiment;
+        }
+    }
+
+}
